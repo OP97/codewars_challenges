f503646267c592c3244b41b6c9f20faa4de644b9
Orbey Paltacı <orbeypaltaci@gmail.com>
2021-04-25 02:52:08 +0300

    Update BitCounting.py

--- BitCounting.py
+++ BitCounting.py
@@ -3,7 +3,7 @@ def count_bits(n):  # week 1
     while n > 0:
         if n & 1:  # check if "n (and) 1" is true
             bits += 1
-        n >>= 1  # Shift two bits right
+        n >>= 1  # Shift bits right
     return bits
 
 print(count_bits("1234"))  # 1234 is 10011010010 number of bits = 5
